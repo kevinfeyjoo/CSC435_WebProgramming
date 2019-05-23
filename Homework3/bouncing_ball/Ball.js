499be84f0d699d7c8f019e83b5311ed29f8e36ca
Kevin Feyjoo <kf9979a@student.american.edu>
2019-05-23 13:29:45 -0400

    uodate

--- Homework3/bouncing_ball/Ball.js
+++ Homework3/bouncing_ball/Ball.js
@@ -6,17 +6,22 @@ window.onload = function ballBounce() {
   ball.style.top = y_pos + "px"; //starts at top
   ball.style.left = 300 - 20 + "px"; //600 by 40 area. This puts it in the middle.
   setInterval(update, 15);
+  setTimeout(ballStop, 500);
 };
 
 function update() {
   var ball = document.getElementById("ball");
   ball.style.top = y_pos + "px";
-  y_pos = Math.min(y_pos + speed, 365); //ball will come to complete stop
+  y_pos = Math.min(y_pos + speed, 365); //365 is the bottom of the box
   speed += 1;
+  let x = 4; //this will increase to slow the ball down
+  x += .1;
 
   if (y_pos >= 360) {
-    speed = -2.5 * speed / 4;//the ball will slow down
-  }else {
-     // make ball stop
-  }
-}
+    speed *= -2.5 / x; //the ball will slow down
+  };
+};
+
+function ballStop(){
+  clearInterval(update);
+};
