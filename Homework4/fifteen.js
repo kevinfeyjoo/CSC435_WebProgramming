7afe68b535337792fd7b824cfd56ece4b8d42b1c
Kevin Feyjoo <kf9979a@student.american.edu>
2019-05-27 09:56:27 -0400

    updates
    using javascript to add style

--- Homework4/fifteen.js
+++ Homework4/fifteen.js
@@ -5,7 +5,7 @@ last updated May 25, 2019
 
 */
 (function() {
-	"use strict";
+	'use strict';
 
 
 window.onload = function(){
@@ -31,36 +31,32 @@ function __init__(){
         '-100px -300px',	
         '-200px -300px'	
     ];
-    let backgorund = document.getElementById("puzzlearea");
-    //backgorund.style.backgroundImage = "URL('background.jpg')";
+    let backgorund = document.getElementById('puzzlearea');
+    backgorund.style.backgroundImage = 'URL("background.jpg")';
 
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
-            let tile = document.createElement("div");
-            tile.id = "tile-" + i + "-" + j;
-            tile.style.top = (i*102+122)+'px';
+            let tile = document.createElement('div');
+            tile.id = 'tile-' + i + '-' + j;
+            tile.style.top = (i*102+128)+'px';
             tile.style.left = (j*1+100*j+1)+'px';
+            tile.style.positiom = 'absolute';
+            tile.style.display = 'block';
+            tile.style.width = '100px';
+            tile.style.height = '100px';
             //tile.style.backgroundPosition = positions[i];
-
-            
+            tile.style.border = '2px solid black';
             backgorund.appendChild(tile);
-
-
-
-
-
             
-
-            
-        }      
-    }
-}
+        };      
+    };
+};
 
 function moveable(){
-    let backgorund = document.getElementById("puzzlearea");
+    let backgorund = document.getElementById('puzzlearea');
     let movable = false;
     
-    let neighbors = "neighbor of" ;
+    let neighbors = 'neighbor of' ;
 /* 
 find neighbors of blank tile. return a list of 4 elements (or 2 if in a corner). return a boolean value to move function. 
  */
@@ -69,7 +65,7 @@ find neighbors of blank tile. return a list of 4 elements (or 2 if in a corner).
 
 
 function move(){
-    let backgorund = document.getElementById("puzzlearea");
+    let backgorund = document.getElementById('puzzlearea');
 /* 
 
    */  
@@ -80,8 +76,9 @@ function move(){
  
 function shuffle() {
     var shuffle = document.getElementById('shufflebutton');
-    let test = document.querySelectorAll("#puzzlearea div");
-    console.log(test);
+    let test = document.querySelectorAll('#puzzlearea div');
+    test[15].className = 'blank';
+    console.log(test[15].classList);
     //shuffle.onclick = console.log(positions);
 /* run 1,000 times */
 };
