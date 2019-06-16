0fc3a96a959e33e389ed7eec0563ac43ff8f4b38
Kevin Feyjoo <kf9979a@student.american.edu>
2019-06-15 20:36:20 -0400

    update

--- Homework4/fifteen.js
+++ Homework4/fifteen.js
@@ -4,18 +4,19 @@
 *   last updated June 10, 2019
 *
 */
-(function() {
-	'use strict';
+(function () {
+    'use strict';
     let backgorund = document.getElementById('puzzlearea');
     var movable = false;
 
-    window.onload = function(){
+    window.onload = function () {
         let controls = document.getElementById('controls');
         __init__();
         shuffle();
+        moveabe();
     };
 
-    function __init__(){
+    function __init__() {
         let backgorund = document.getElementById('puzzlearea');
         backgorund.innerHTML = "";
         backgorund.style.backgroundImage = 'URL("grinch4.png")';
@@ -25,18 +26,21 @@
             for (let j = 0; j <= 3; j++) {
                 let tile = document.createElement('span');
                 tile.id = 'tile-' + i + '-' + j;
-                tile.style.top = (i*100)+'px';
-                tile.style.left = (j*1+99*j)+'px';
-                
-                
-                
+                tile.style.top = (i * 100) + 'px';
+                tile.style.left = (j * 1 + 99 * j) + 'px';
+
+
+
                 /* ¬ßeføre s†¥le iπ js. s†¥le iπ css … ®*/
-                
+
                 tile.style.position = 'absolute';
                 tile.style.display = 'block';
                 tile.style.width = '100px';
                 tile.style.height = '100px';
-                tile.style.border = '2px solid black';
+                tile.style.border = '4px solid black';
+
+                /* _______________________________________ */
+
                 if (number <= 15) {
                     tile.classList.add('number');
                     tile.innerHTML = (number++).toString();
@@ -45,30 +49,35 @@
                 }
                 backgorund.appendChild(tile);
 
+                /* _______________________________________ */
 
-                tile.onclick = function(){
-                    let tiles = {
-                        left: tile.style.left,
-                        top: tile.style.top
-                    };
-                    //console.log("(Left, Top): "+"("+left +","+ top+")"+" Class Name --> "+classs);
-                    console.log(tiles)
-
-                }   
-            }      
+                tile.onclick = function () {
+                    moveabe();
+                }
+            }
         }
     }
+
+    function moveabe() {
+        //
+        let tiles = document.querySelectorAll('#puzzlearea span');
+        let empty = document.querySelectorAll('#puzzlearea .blank');
+
+        console.log(tiles[15]);
+        console.log(empty);
+    }
+
 
     function shuffle() {
         var shuffle = document.getElementById('shufflebutton');
         let tile = document.querySelectorAll('#puzzlearea span');
         let empty = document.querySelectorAll('#puzzlearea .blank');
-        shuffle.onclick = function(){
+        shuffle.onclick = function () {
             console.log(empty);
-            
+
         }
- 
-    /* run 1,000 times */
+
+        /* run 1,000 times */
     };
-    
+
 })();
