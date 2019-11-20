a638f403eafd86258e6421cf817b08423290707b
Kevin Feyjoo <kf9979a@student.american.edu>
2019-11-20 01:03:15 -0500

    Update fifteen.js

--- Homework4/fifteen.js
+++ Homework4/fifteen.js
@@ -9,66 +9,72 @@
     window.onload = function () {
         let controls = document.getElementById('controls');
         __init__();
-        clearInterval(__init__);
         shuffle();
     };
 
     function __init__() {
         let background = document.getElementById('puzzlearea');
         background.innerHTML = "";
+        
+        console.log(1);
 
         var number = 1;
-        for (let row = 0; row <= 3; row++) {
-            for (let col = 0; col <= 3; col++) {
+        let doneOnce = true;
 
-                /* _______create tiles___________ */
-                let tile = document.createElement('span');
-                tile.id = 'tile-' + row + '-' + col;
-                tile.style.top = (row * 98) + 'px';
-                tile.style.left = (col + 96 * col) + 'px';
-                tile.style.backgroundImage = 'URL("background1.jpeg")';
-
-                /*______________S†¥le______________*/
-
-                tile.style.position = 'absolute';
-                tile.style.display = 'block';
-                tile.style.width = '90px';
-                tile.style.height = '90px';
-                tile.style.border = '4px solid black';
-
-                /* ____________Add Number___________ */
-                if (row*col == 9) {
-                    tile.style.backgroundImage = 'none';
-                    tile.style.border = 'none';
-                }
-
-
-
-
-                if (number <= 15) {       //ååååååBUGGGGGGGGGGGG
-                    tile.classList.add('number');
-                    tile.style.backgroundPositionX = (-1*col*100) + 'px';
-                    tile.style.backgroundPositionY = (-1*row*100) + 'px';
-                    tile.innerHTML = (number++).toString();
-                } else {
-                    tile.innerHTML = '';
-                    tile.className = 'blank';              
-
-                }                       //ååååååBUGGGGGGGGGGGG
-
-
-
-                
-                /* _________Add tiles to screen________ */
-                
-                background.appendChild(tile);
-
-                /* _____________Moveable____________ */
-
-                tile.onclick = function (e) {
-                    moveabe(e.target);//returns tile that 'triggered the event'
+        if (doneOnce){
+            for (let row = 0; row <= 3; row++) {
+                for (let col = 0; col <= 3; col++) {
+    
+                    /* _______create tiles___________ */
+                    let tile = document.createElement('span');
+                    tile.id = 'tile-' + row + '-' + col;
+                    tile.style.top = (row * 98) + 'px';
+                    tile.style.left = (col + 96 * col) + 'px';
+                    tile.style.backgroundImage = 'URL("background1.jpeg")';
+    
+                    /*______________S†¥le______________*/
+    
+                    tile.style.position = 'absolute';
+                    tile.style.display = 'block';
+                    tile.style.width = '90px';
+                    tile.style.height = '90px';
+                    tile.style.border = '4px solid black';
+    
+                    /* ____________Add Number___________ */
+                    if (row*col == 9) {
+                        tile.style.backgroundImage = 'none';
+                        tile.style.border = 'none';
+                    }
+    
+    
+    
+    
+                    if (number <= 15) {       //ååååååBUGGGGGGGGGGGG
+                        tile.classList.add('number');
+                        tile.style.backgroundPositionX = (-1*col*100) + 'px';
+                        tile.style.backgroundPositionY = (-1*row*100) + 'px';
+                        tile.innerHTML = (number++).toString();
+                    } else {
+                        tile.innerHTML = '';
+                        tile.className = 'blank';              
+    
+                    }                       //ååååååBUGGGGGGGGGGGG
+    
+    
+    
+                    
+                    /* _________Add tiles to screen________ */
+                    
+                    background.appendChild(tile);
+    
+                    /* _____________Moveable____________ */
+    
+                    tile.onclick = function (e) {
+                        moveabe(e.target);//returns tile that 'triggered the event'
+                    }
                 }
             }
+            doneOnce = false;
         }
     }
 
@@ -79,6 +85,8 @@
 
             // moves clicked tile to blank spot by switching id's
             //CURRENT BUG == ROW-COL 3-3 will not display anything but BLANK
+
+            //Background positions must be traded as well
             tile.id = blankTile.id;
             tile.style = blankTile.style;
             blankTile.id = placeHolder.id;
