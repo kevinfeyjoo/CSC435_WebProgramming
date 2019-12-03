e8bc9d40f553ff4ac8e0885668e92370b032d728
Kevin Feyjoo <kf9979a@student.american.edu>
2019-12-02 23:05:21 -0500

    a

--- 1HomePage/Projects/resume/main.js
+++ 1HomePage/Projects/resume/main.js
@@ -1,11 +1,4 @@
 (function () {
     'use strict';
-    var test = document.querySelectorAll(".w3.quarter");
-    for (let i = 1; i < test.length; i++) {
-        console.log(test[i]);
-    };
-    
-    
-
-
+    alert('this website is still under construction. please excuse any typos or bugs');
 })();
