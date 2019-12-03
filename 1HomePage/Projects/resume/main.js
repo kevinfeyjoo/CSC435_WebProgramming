a7e7c46b088da9255e80c195246157a15b26afd1
Kevin Feyjoo <kf9979a@student.american.edu>
2019-12-02 23:46:52 -0500

    updates

--- 1HomePage/Projects/resume/main.js
+++ 1HomePage/Projects/resume/main.js
@@ -1,4 +1,11 @@
 (function () {
     'use strict';
     alert('this website is still under construction. please excuse any typos or bugs');
+
+/* 
+
+    when click on project box. make it come out for more details, images and a chance to visit the website.
+
+    do this with event listener
+     */
 })();
