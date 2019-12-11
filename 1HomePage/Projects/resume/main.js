af0e1fb0c1d391873cc872ae7d9d0e52790b5147
Kevin Feyjoo <kf9979a@student.american.edu>
2019-12-11 01:15:53 -0500

    updates

--- 1HomePage/Projects/resume/main.js
+++ 1HomePage/Projects/resume/main.js
@@ -1,20 +1,20 @@
 (function () {
     'use strict';
-    /* alert('this website is still under construction. please excuse any typos or bugs'); */
 
     let projects = document.querySelectorAll('.w3-quarter');
     let p_len = projects.length;
+    let modals = document.getElementsByClassName('modal');
+    let closeBtn = document.getElementsByClassName('closeBtn');
+
 
     for (let i = 0; i < p_len; i++) {
+        //click on project to display modal
         projects[i].addEventListener('click', function pop() {
-            //console.log(projects[i].childNodes);
+            modals[i].style.display = 'block';
+            closeBtn[i].addEventListener('click', function close() {
+                modals[i].style.display = 'none';
+            })
         });
-        
     }
-/* 
 
-    when click on project box. make it come out for more details, images and a chance to visit the website.
-
-    do this with event listener
-     */
 })();
