c7d1bb8f28e3e3c2bd9c2ebfda975b49e0df14f2
Kevin Feyjoo <kf9979a@student.american.edu>
2019-12-11 01:37:04 -0500

    updates

--- 1HomePage/Projects/resume/main.js
+++ 1HomePage/Projects/resume/main.js
@@ -6,11 +6,13 @@
     let modals = document.getElementsByClassName('modal');
     let closeBtn = document.getElementsByClassName('closeBtn');
 
+     
 
     for (let i = 0; i < p_len; i++) {
         //click on project to display modal
         projects[i].addEventListener('click', function pop() {
             modals[i].style.display = 'block';
+            
             closeBtn[i].addEventListener('click', function close() {
                 modals[i].style.display = 'none';
             })
