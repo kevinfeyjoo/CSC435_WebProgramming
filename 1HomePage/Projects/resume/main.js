1c03f68a32653106e80f5a47e1fc8e8a40dd1fa1
Kevin Feyjoo <kf9979a@student.american.edu>
2019-12-15 01:34:06 -0500

    updates

--- 1HomePage/Projects/resume/main.js
+++ 1HomePage/Projects/resume/main.js
@@ -6,17 +6,27 @@
     let modals = document.getElementsByClassName('modal');
     let closeBtn = document.getElementsByClassName('closeBtn');
 
+    //toggle modal 
     for (let i = 0; i < p_len; i++) {
         //click on project to display modal
         projects[i].addEventListener('click', function pop() {
             modals[i].style.display = 'block';
             
+
+            //window.addEventListener('click', function close() {
             closeBtn[i].addEventListener('click', function close() {
                 modals[i].style.display = 'none';
             })
         });
     }
 
-
-
+    //add a function to exit modal if click outside the modal area
+    //this implementation needs work... tryh this https://sabe.io/tutorials/how-to-create-modal-popup-box
+    function clickWindow(){     
+        for (let i = 0; i < p_len; i++) {
+            if (modals[i].style.display == 'block') {
+                window.addEventListener('click', modals[i].style.display = 'none');
+            }
+        }
+    }
 })();
